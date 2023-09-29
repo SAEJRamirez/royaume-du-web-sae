422903291079f9ee208aa03beb159482057aade6
Julien <julien.ramirez@gmail.com>
2023-09-29 10:53:52 +0200

    add style and logos

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const colors = require("colors")
 const dotenv = require("dotenv").config()
 const cors = require("cors")
-const port = process.env.PORT || 5000
+const port = process.env.PORT
 const connectDB = require("./config/db")
 
 connectDB()
